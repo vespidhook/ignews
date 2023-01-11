782d7ba31c4d89f4bd63a0e1fa4eca14275069e9
Bruno Alves <vespidhook@gmail.com>
2023-01-11 18:31:40 -0300

    arrumando app

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
-import '../styles/globals.css'
-
-export default function App({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
+export default MyApp
