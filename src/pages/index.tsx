89ead5841e121fbb31ed6a8b3b3d94663b27c992
Bruno Alves <vespidhook@gmail.com>
2023-01-12 00:31:10 -0300

    css clobal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
+import Head from 'next/head';
 import styles from '../styles/home.module.scss';
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>Hello World</h1>
+    <>
+      <Head>
+        <title>Incio | ig.news</title>
+      </Head>
+      <h1 className={styles.title}>
+        Hello <span>World</span>
+      </h1>
+    </>
   )
 }
